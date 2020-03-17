3b938012a02fbef6500fcb5d54266755a9ceaee9
Garbua <denisgarbuza@yandex.ru>
2020-03-17 22:29:48 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let number = n;
+    if(n < 0){
+        number *= -1;
+    }
+    let rev = Number(String(number).split('').reverse().join(''));
+  return rev;
 }
